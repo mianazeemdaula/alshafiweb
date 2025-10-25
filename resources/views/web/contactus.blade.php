2de89436de22d9ac1a9ff3269f8add101be8e19d
mianazeemdaula <mianazeemdaula@gmail.com>
2025-10-25 11:11:27 +0500

    3235

--- resources/views/web/contactus.blade.php
+++ resources/views/web/contactus.blade.php
@@ -50,33 +50,34 @@
                     <div class="flex items-center mb-2">
                         <i class="fa fa-phone mx-1 text-gray-500 dark:text-gray-400"></i>
                         <div class="text-gray-700 dark:text-gray-300">
-                            {{ __('contactus.helpline') ?? 'Helpline +92-322-3232727' }}</div>
+                            {{ 'Helpline +92-322-3232727' }}</div>
                     </div>
                     <div class="border border-slate-100 dark:border-gray-700 h-4 mb-2"></div>
                     <div class="flex items-center mb-2">
                         <i class="fa fa-envelope mx-1 text-gray-500 dark:text-gray-400"></i>
                         <div>
                             <a href="mailto:info@alshaafionline.com"
-                                class="hover:text-blue-600 dark:hover:text-blue-400 text-gray-700 dark:text-gray-300">info@alshafi.com</a>
+                                class="hover:text-blue-600 dark:hover:text-blue-400 text-gray-700 dark:text-gray-300">info@alshaafionline.com</a>
                         </div>
                     </div>
                     <div class="border border-slate-100 dark:border-gray-700 h-4 mb-2"></div>
                     <div class="flex items-center">
                         <i class="fa fa-map-marker-alt mx-1 text-gray-500 dark:text-gray-400"></i>
                         <div class="text-gray-700 dark:text-gray-300">
-                            {{ __('contactus.address') ?? 'Address: Alshaafi Dawakhna, MI City, Okara Road, Depalpur' }}
+                            {{ 'Address: Alshaafi Dawakhna, MI City, Okara Road, Depalpur' }}
                         </div>
                     </div>
                 </div>
                 <div
                     class="mt-4 p-6 bg-white dark:bg-gray-800 rounded-lg shadow-md border border-gray-200 dark:border-gray-700">
                     <div class="text-lg font-medium text-gray-900 dark:text-gray-100 mb-3">
-                        {{ __('contactus.location') ?? 'Location' }}
+                        {{ 'Location' }}
                     </div>
                     <div class="w-full h-64 rounded overflow-hidden">
-                        <iframe src="https://maps.app.goo.gl/ZcXfob6ugZLpMsdP8?g_st=ic&output=embed" width="100%"
-                            height="100%" frameborder="0" style="border:0;" allowfullscreen loading="lazy"
-                            referrerpolicy="no-referrer-when-downgrade" title="Alshaafi location"></iframe>
+                        <iframe
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3431.515529108475!2d73.645788!3d30.6757681!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x391805422bafa6d3%3A0xcd5a79457e9ba657!2zQUxTSEFBRkkgREFXQUtIQU5BIHwg2KfZhNi02KfZgduMINiv2YjYp9iu2KfZhtuB!5e0!3m2!1sen!2s!4v1761372606921!5m2!1sen!2s"
+                            width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+                            referrerpolicy="no-referrer-when-downgrade"></iframe>
                     </div>
                 </div>
             </div>
