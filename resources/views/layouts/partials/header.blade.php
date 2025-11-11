b48198a5d356a6c28b700b830cabeeeae7afb734
mianazeemdaula <mianazeemdaula@gmail.com>
2025-11-11 11:00:02 +0500

    3232

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -9,7 +9,7 @@
         <div class="border border-white/30 h-4 hidden sm:block"></div>
         <div class="flex items-center text-white hover:text-gray-100 transition-colors">
             <i class="fa fa-phone mx-1 animate-pulse"></i>
-            <div class="hidden sm:block font-medium">Helpline +923253255555</div>
+            <div class="hidden sm:block font-medium">Helpline +92 325 325 55 55</div>
         </div>
     </div>
     <div class="flex space-x-3 items-center">
