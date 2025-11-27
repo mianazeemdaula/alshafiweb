6a9c42af5853875f6251de82fbd69defa22ff54f
mianazeemdaula <mianazeemdaula@gmail.com>
2025-11-27 11:09:46 +0500

    32

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -23,7 +23,7 @@ class WebController extends Controller
             }
         }
         
-        $products = $products->take(10)->inRandomOrder()->get();
+        $products = $products->take(10)->where('featured', 1)->inRandomOrder()->get();
         
         // Get recent blog posts for current country
         $blogPosts = \App\Models\BlogPost::with(['country', 'category']);
